5591005a1b36f91d2470f2956b5eb72e484fb7c7
Muhammed Safwaan Ally <ST10352114@vcconnect.edu.za>
2025-04-19 11:50:47 +0200

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystem2
+{
+    abstract class Order
+    {
+        public int OrderID { get; set; }
+        public double Amount { get; set; }
+
+        public abstract void ProcessPayment();
+
+        public virtual void PrintReceipt()
+        {
+            Console.WriteLine($"Order {OrderID}, Amount Paid: {Amount}");
+
+        }
+
+        class OnlineOrder : Order
+        {
+            public override void ProcessPayment()
+            {
+                Console.WriteLine($"Processing online payment for Order: {OrderID}");
+            }
+
+            public override void PrintReceipt()
+            {
+                Console.WriteLine($"Printing receopt for Order:{OrderID}");
+                base.PrintReceipt();
+            }
+        }
+
+        class InPersonOrder : Order
+        {
+            public override void ProcessPayment()
+            {
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderingSystem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //hello1
+        }
+    }
+}
